67e04bfc949d2dc6c1a615a9ca0b86f6ea1a6c39
Rob Beir, de <rob.de.beir@boskalis.com>
2020-06-03 14:44:57 +0200

    Kill ExpressionChangedAfterItHasBeenCheckedError bug

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Person, DataService } from './data.service';
 
@@ -7,16 +7,27 @@ import { Person, DataService } from './data.service';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
+export class AppComponent implements OnInit, AfterViewInit {
   title = 'ng-select';
 
   people$: Observable<Person[]>;
-  selectedPersonId = '5a15b13c36e7a7f00cf0d7cb';
+  // selectedPersonId = '5a15b13c36e7a7f00cf0d7cb';
+  selectedPersonId: string;
 
   constructor(private dataService: DataService) {
   }
 
   ngOnInit() {
     this.people$ = this.dataService.getPeople();
+
+    setTimeout(() => {
+      this.selectedPersonId = '5a15b13c36e7a7f00cf0d7cb';
+    }, 0);
   }
+
+  ngAfterViewInit(): void {
+
+  }
+
+
 }
--- src/app/demo.component.ts
+++ src/app/demo.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Person, DataService } from './data.service';
 
@@ -7,15 +7,24 @@ import { Person, DataService } from './data.service';
   templateUrl: './demo.component.html',
   styleUrls: ['./demo.component.css']
 })
-export class DemoComponent implements OnInit {
+export class DemoComponent implements OnInit, AfterViewInit {
 
   people$: Observable<Person[]>;
-  selectedPersonId = '5a15b13c36e7a7f00cf0d7cb';
+  // selectedPersonId = '5a15b13c36e7a7f00cf0d7cb';
+  selectedPersonId: string;
 
   constructor(private dataService: DataService) {
   }
 
   ngOnInit() {
     this.people$ = this.dataService.getPeople();
+
+    setTimeout(() => {
+      this.selectedPersonId = '5a15b13c36e7a7f00cf0d7cb';
+    }, 0);
+  }
+
+  ngAfterViewInit(): void {
+
   }
 }
